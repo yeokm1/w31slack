f6cf1def97ceb37ffe7176538454b7d88bad44e1
Yeo Kheng Meng <yeokm1@gmail.com>
2019-12-24 22:48:18 +0800

    channels parsing also now have more stringent checks

--- app/jsnparse/jsnparse.c
+++ app/jsnparse/jsnparse.c
@@ -5,7 +5,6 @@
 #include "stdio.h"
 
 #define MAX_TOKENS 256
-#define MAX_TOKEN_KEYWORD_SIZE 20
 
 //Jump straight to the Json portion skipping the HTTP content
 static void jsnparse_getStartOfJson(LPSTR response, DWORD responseLength, LPSTR * startPosition, DWORD * lengthOfJson){
@@ -109,12 +108,8 @@ void jsnparse_parseChannelList(LPSTR response, DWORD length, ChannelList * list)
     LPSTR startOfJson;
     DWORD lengthOfJson;
 
-    int tokenStartPosition;
-    int tokenSize;
-    jsmntok_t currentToken;
-    char currentTokenString[MAX_TOKEN_KEYWORD_SIZE];
-    int numberOfChannels = 0;
-    int channelIndex = 0;
+    char * currentTokenString;
+    char * currentTokenStringp2;
 
     jsnparse_getStartOfJson(response, length, &startOfJson, &lengthOfJson);
 
@@ -123,70 +118,33 @@ void jsnparse_parseChannelList(LPSTR response, DWORD length, ChannelList * list)
 
     //First pass to determine number of channels there are
     for(index = 0; index < MAX_TOKENS; index++){
-        currentToken = tokens[index];
-        tokenStartPosition = currentToken.start;
-        tokenSize = currentToken.end - tokenStartPosition;
 
-        if(tokenSize <= 0){
-            continue;
-        }
+        currentTokenString = jsnparse_extractStringOfThisToken(startOfJson, &tokens[index]);
 
-        if(tokenSize > MAX_TOKEN_KEYWORD_SIZE){
-            continue;
-        }
-
-        memset(currentTokenString, 0, MAX_TOKEN_KEYWORD_SIZE);
-        _fmemcpy(currentTokenString, startOfJson + tokenStartPosition, tokenSize);
-
-        //We found the start of a nested json for a channel
         if(strcmp("id", currentTokenString) == 0){
 
             //Verify that the channel ID and name does not exceed the tokens we have
             if(((index + 1) < MAX_TOKENS) && ((index + 3) < MAX_TOKENS)){
-                numberOfChannels++;
+
+                //Make sure [index + 2] token is "user" as "text" and "user" must come in a pair
+                currentTokenStringp2 = jsnparse_extractStringOfThisToken(startOfJson, &tokens[index + 2]);
+
+                if(strcmp("name", currentTokenStringp2) == 0){
+
+                    list->channels = (Channel * ) realloc(list->channels, (list->numChannels + 1) * sizeof(Channel));
+
+                    list->channels[list->numChannels].channelID = jsnparse_extractStringOfThisToken(startOfJson, &tokens[index + 1]);
+                    list->channels[list->numChannels].channelName = jsnparse_extractStringOfThisToken(startOfJson, &tokens[index + 3]);
+
+                    list->numChannels++;
+                }
+
+                free(currentTokenStringp2);
+                
             }
 
         }
-    }
-
-    list->channels = (Channel * ) malloc(numberOfChannels * sizeof(Channel));
-    list->numChannels = numberOfChannels;
-
-
-    for(index = 0; index < MAX_TOKENS; index++){
-        currentToken = tokens[index];
-        tokenStartPosition = currentToken.start;
-        tokenSize = currentToken.end - tokenStartPosition;
-
-        if(tokenSize <= 0){
-            continue;
-        }
-
-        if(tokenSize > MAX_TOKEN_KEYWORD_SIZE){
-            continue;
-        }
-
-        memset(currentTokenString, 0, MAX_TOKEN_KEYWORD_SIZE);
-        _fmemcpy(currentTokenString, startOfJson + tokenStartPosition, tokenSize);
-
-        //We found the start of a nested json for a channel
-        if(strcmp("id", currentTokenString) == 0){
-
-            //Verify that the channel ID and name does not exceed the tokens we have
-            if(((index + 1) < MAX_TOKENS) && ((index + 3) < MAX_TOKENS)){
-                
-                // Copy the data to the return structure
-                list->channels[channelIndex].channelID = (char *)calloc((tokens[index + 1].end - tokens[index + 1].start + 1), sizeof(char));
-                _fmemcpy(list->channels[channelIndex].channelID, startOfJson + tokens[index + 1].start, tokens[index + 1].end - tokens[index + 1].start);
-                
-                list->channels[channelIndex].channelName = (char *)calloc((tokens[index + 3].end - tokens[index + 3].start + 1), sizeof(char));
-                _fmemcpy(list->channels[channelIndex].channelName, startOfJson + tokens[index + 3].start, tokens[index + 3].end - tokens[index + 3].start);
-
-                channelIndex++;
-
-            }
-
-        }
+        free(currentTokenString);
     }
 }
 
